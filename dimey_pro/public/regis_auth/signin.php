d512a7e7a1d3df99fd5894b081eee5a7747aec95
Dmitro  Pohrebennyi <149794693+Dimeydimonov@users.noreply.github.com>
2024-10-30 15:48:59 +0200

    Update signin.php

--- dimey_pro/public/regis_auth/signin.php
+++ dimey_pro/public/regis_auth/signin.php
@@ -6,7 +6,7 @@ require_once  'connect.php';
 $login = $_POST['login'];
 $password = md5($_POST['password']);
 
-$check_user = mysqli_query($connect, "SELECT * FROM test2 WHERE username='$login' AND password='$password'");
+$check_user = mysqli_query($connect, "SELECT * FROM test WHERE username='$login' AND password='$password'");
  if(mysqli_num_rows($check_user)>0) {
 
      $user = mysqli_fetch_assoc($check_user);
